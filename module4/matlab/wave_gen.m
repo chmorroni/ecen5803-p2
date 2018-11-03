1f6f379c59551061c3571b202a10d8b4a922bf18
Christopher Morroni <chmorroni@gmail.com>
2018-11-03 12:01:10 -0600

    added simulink and updated matlab script

--- module4/matlab/wave_gen.m
+++ module4/matlab/wave_gen.m
@@ -1,12 +1,18 @@
+% parameters
 F = 1004;
-Fs = 20000;
+Fs = 192000;
 Ts = 1/Fs;
 num_periods = 251;
-%num_periods = 1;
 len = Fs / F * num_periods;
 
+% generate wave
 t = [0:len] * Ts;
 wave = 1.65 * sin(2 * pi * F * t) + 1.65;
 
+% output to file
+csvwrite("waveform.csv", transpose(wave));
+
+% generate C array for lookup table
 array = num2str(wave, '%1.6ff, %1.6ff, %1.6ff, %1.6ff, %1.6ff, %1.6ff, %1.6ff, %1.6ff, %1.6ff, %1.6ff,\n');
 join(['{' array '}'])
+len
